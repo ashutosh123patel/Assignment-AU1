73914fed34bfd0706272b37a2d7134ff0b1a9ba6
ashutosh123patel <145385513+ashutosh123patel@users.noreply.github.com>
2023-12-10 20:13:05 +0530

    Add files via upload

---
+++ Percentage of the marks of 5 subjects.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+  int main ()
+  {
+	  float m1 = 96; //  maths mark
+	  float m2= 97;	// physics marks
+      float m3= 89;	// hindi marks
+	  float m4= 90;	// english
+	  float m5 = 91; //chemistry
+	  float percentage =  ( m1+m2+m3+m4+m5)/5;
+
+	  printf("THE PERCENTAGE OF 5 SUBJECTS IS:");	 
+	  printf("%f",percentage );
+      
+	  return 0 ;
+ }
