c4151ff6f6ef8cb90b09d20abd4d73eab8a41289
ashutosh123patel <145385513+ashutosh123patel@users.noreply.github.com>
2023-12-10 20:10:55 +0530

    Add files via upload

---
+++ Arithmetic operations on int data type.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+  int main ()
+  {
+	  int x = 986;
+	  int y = 387;			 
+	  printf("%d",x+y);
+      printf("\n%d",x-y);
+      printf("\n%d",x*y);
+	  printf("\n%d",x/y);
+	  return 0 ;
+	  }
