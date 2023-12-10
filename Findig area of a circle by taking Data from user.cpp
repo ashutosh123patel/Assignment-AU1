a066e788768723110d3df240b08c113fb957ca79
ashutosh123patel <145385513+ashutosh123patel@users.noreply.github.com>
2023-12-10 20:11:23 +0530

    Add files via upload

---
+++ Findig area of a circle by taking Data from user.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+  int main ()
+  {
+	  float Radius;
+	  printf("ENTER RADIUS:");
+	  scanf ("%f",&Radius);
+	   float  pi= 3.1415;
+	  float area = (Radius*Radius*pi);
+	  printf("THE AREA OF CIRCLE IS :");
+	  
+	  printf("%f", area );
+      
+	  return 0 ;
+ }
