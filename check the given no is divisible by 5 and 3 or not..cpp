ae61193681a745519d461ce8d11cd09326bc9972
ashutosh123patel <145385513+ashutosh123patel@users.noreply.github.com>
2023-12-14 16:50:25 +0530

    Add files via upload

---
+++ check the given no is divisible by 5 and 3 or not..cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{  int x;
+   printf("enter a number ");
+   scanf("%d",&x); 
+  
+   if (x%5==0 || x%3==0) 
+   printf("given no is  divisble by 5 or 3 ");
+   
+   else
+    
+    printf("given no is not divisble by 5 or  3 ");
+   
+ 
+	return 0;
+}
