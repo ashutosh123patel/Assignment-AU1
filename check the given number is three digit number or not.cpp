8a7c255df2763ffa145e5b4b9ec01bf2596f1268
ashutosh123patel <145385513+ashutosh123patel@users.noreply.github.com>
2023-12-14 16:51:00 +0530

    Add files via upload

---
+++ check the given number is three digit number or not.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{  int x;
+   printf("enter a number ");
+   scanf("%d",&x);
+  
+   if (x>99 && x<1000) 
+   printf("three digit nnumber ");
+   
+   else
+    printf(" not a three digit number");
+   
+ 
+	return 0;
+}
